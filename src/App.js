641064f83b399ed3bea8cdce0eac19d214e8dd11
David Casanellas <davidcasanell@gmail.com>
2018-06-19 19:10:20 +0200

    feat: dispatch fetch cards action in App.js constructor + promote store Provider to index.js

--- src/App.js
+++ src/App.js
@@ -1,70 +1,76 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-import store from './store';
+import { fetchCards } from './actions/cardActions';
 
 import Card from './containers/card/card';
 import logo from './logo.svg';
 import './App.css';
 
-import json from './mock/cards.json';
-
 class App extends Component {
   constructor(props) {
     super(props);
+
+    // fetch cardSet
+    this.props.fetchCards();
 
     // isLoaded is set to false when the HS API is used and sets property is uncommented
     this.state = {
       error: false,
       isLoaded: true,
-      // sets: undefined
-      sets: json,
     };
-
-    // needs to dispatch de fetchCards action
   }
 
   render() {
     if (!this.state.isLoaded) {
       return (
-        <Provider store={store}>
-          <div className="App">
-            <div className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h2>Welcome to React Hearthstone</h2>
-            </div>
-            <p>Loading ... </p>
-          </div>
-        </Provider>
-      );
-    }
-    return (
-      <Provider store={store}>
         <div className="App">
           <div className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h2>Welcome to React Hearthstone</h2>
           </div>
-          <div className="heroes">
-            {Object.keys(this.state.sets).map(nameSet =>
-              this.state.sets[nameSet]
-                .filter(set => set.type === 'Hero' && set.cardSet === 'Basic')
-                .map((card, i) => <Card key={i} card={card} />),
-            )}
-          </div>
-          <div>
-            {/* How to paint ALL cards */}
-            {/*
-            {Object.keys(this.state.sets)
-              .map(item => this.state.sets[item].map((card, i) => <Card key={i} card={card} />))}
-            */}
-            {/* How to paint an specific collection */}
-            {/* {this.state.sets.Naxxramas.map((card, i) => <Card key={i} card={card} />)} */}
-          </div>
+          <p>Loading ... </p>
         </div>
-      </Provider>
+      );
+    }
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React Hearthstone</h2>
+        </div>
+        <div className="heroes">
+          {Object.keys(this.props.cards).map(nameSet =>
+            this.props.cards[nameSet]
+              .filter(set => set.type === 'Hero' && set.cardSet === 'Basic')
+              .map((card, i) => <Card key={i} card={card} />),
+          )}
+        </div>
+        <div>
+          {/* How to paint ALL cards */}
+          {/*
+            {Object.keys(this.props.cards)
+              .map(item => this.props.cards[item].map((card, i) => <Card key={i} card={card} />))}
+            */}
+          {/* How to paint an specific collection */}
+          {/* {this.props.cards.Naxxramas.map((card, i) => <Card key={i} card={card} />)} */}
+        </div>
+      </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  fetchCards: PropTypes.func.isRequired,
+  cards: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  cards: state.cardReducer.cards,
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchCards },
+)(App);

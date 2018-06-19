11385ecd0bb6126ba50606a823e5d6f035e23896
David Casanellas <davidcasanell@gmail.com>
2018-06-19 18:36:11 +0200

    feat: remove axios call from straight away from App component

--- src/App.js
+++ src/App.js
@@ -21,19 +21,7 @@ class App extends Component {
       sets: json,
     };
 
-    // axios.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards?locale=esES',
-    //   { headers:
-    //     { 'X-Mashape-Key': 'GmSwaTQi8VmshabahQuB1zaqxcF4p1DB8CpjsnCHEXjhNhtpaI' },
-    //   })
-    //   .then((response) => {
-    //     this.setState({
-    //       isLoaded: true,
-    //       sets: response.data,
-    //     });
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
+    // needs to dispatch de fetchCards action
   }
 
   render() {

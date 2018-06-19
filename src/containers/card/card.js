8e334c8f91e20f055086aad47fd5413c1e6b0011
David Casanellas <davidcasanell@gmail.com>
2018-06-19 17:47:31 +0200

    feat: add linter rule

---
+++ src/containers/card/card.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Card from '../../components/card/card.js';
+
+class CardContainer extends Component {
+
+  onClick() {
+    alert(this.props.card.name + ' selected!');
+  }
+
+  render() {
+    return (
+      <Card onClick={() => this.onClick.bind(this)} card={this.props.card} />
+    );
+  }
+}
+
+CardContainer.propTypes = {
+  card: PropTypes.object.isRequired
+};
+
+export default CardContainer;
---
+++ src/containers/hero-selector/hero-selector.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import Card from '../../component/card/card.js';
+
+class HeroSelector extends Component {
+
+
+
+  render() {
+
+  }
+}

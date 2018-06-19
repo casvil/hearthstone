cfdf4155de37462db12a3e4d143879926819d123
David Casanellas <davidcasanell@gmail.com>
2018-06-19 18:38:24 +0200

    chore: actions + commented code to be async called

---
+++ src/actions/cardActions.js
@@ -0,0 +1,13 @@
+// axios.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards?locale=esES',
+//   { headers:
+//     { 'X-Mashape-Key': 'GmSwaTQi8VmshabahQuB1zaqxcF4p1DB8CpjsnCHEXjhNhtpaI' },
+//   })
+//   .then((response) => {
+//     this.setState({
+//       isLoaded: true,
+//       sets: response.data,
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
---
+++ src/actions/types.js
@@ -0,0 +1,6 @@
+/** User Types * */
+export const SET_USER_DETAILS_HERO = 'SET_USER_DETAILS_HERO';
+export const SET_USER_DETAILS_USERNAME = 'SET_USER_DETAILS_USERNAME';
+
+/** Card Types * */
+export const FETCH_CARDS = 'FETCH_CARDS';

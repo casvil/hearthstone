60365080422c4dbb9d2294e8d5855818fc6d7eb2
David Casanellas <davidcasanell@gmail.com>
2018-06-20 23:31:50 +0200

    chore: use .env file in order to leverage an API from cards fetch request

--- src/actions/cardActions.js
+++ src/actions/cardActions.js
@@ -1,24 +1,22 @@
 import { FETCH_CARDS } from './types';
+// import json from '../mock/cards.json';
 
-import json from '../mock/cards.json';
+const { REACT_APP_API_KEY } = process.env;
 
-// axios.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards?locale=esES',
-//   { headers:
-//     { 'X-Mashape-Key': 'GmSwaTQi8VmshabahQuB1zaqxcF4p1DB8CpjsnCHEXjhNhtpaI' },
-//   })
-//   .then((response) => {
-//     this.setState({
-//       isLoaded: true,
-//       sets: response.data,
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-export const fetchCards = () => dispatch =>
-  // mocked data
-  dispatch({
-    type: FETCH_CARDS,
-    payload: json,
-  });
+export const fetchCards = () => (dispatch) => {
+  fetch('https://omgvamp-hearthstone-v1.p.mashape.com/cards?locale=esES', {
+    type: 'POST',
+    headers: { 'X-Mashape-Key': REACT_APP_API_KEY },
+  })
+    .then(res => res.json())
+    .then(cardSet =>
+      dispatch({
+        type: FETCH_CARDS,
+        payload: cardSet,
+      }),
+    )
+    .catch((error) => {
+      // do something -> dispatch action error request
+      console.warn(error);
+    });
+};

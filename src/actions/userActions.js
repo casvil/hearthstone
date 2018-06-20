7fef831581d76277c06c84e3f5d5f023e96d4eab
David Casanellas <davidcasanell@gmail.com>
2018-06-20 22:53:58 +0200

    chore: basic login and register actions

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,4 +1,16 @@
+import { push } from 'connected-react-router';
+
 import { SET_USER_DETAILS_HERO, SET_USER_DETAILS_USERNAME } from './types';
+
+export const login = (/* username, password, token */) => (dispatch) => {
+  /* do something before redirection */
+  dispatch(push('/login'));
+};
+
+export const register = (/* formData */) => (dispatch) => {
+  /* do something before redirection */
+  dispatch(push('/register'));
+};
 
 export const setUserDetailsHero = hero => (dispatch) => {
   dispatch({

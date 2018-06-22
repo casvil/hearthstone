b338465cdf0837370c774bc114d3a63d834f792a
David Casanellas <davidcasanell@gmail.com>
2018-06-22 21:24:17 +0200

    feat: connect header to redux to be able to dispatch redux actions

--- src/containers/header/header.js
+++ src/containers/header/header.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { home } from '../../actions/userActions';
 
 import ProfileSection from './profile-section';
+
 import './header.css';
 import logo from '../../logo.svg';
 
 class Header extends Component {
+  handleClick = () => {
+    this.props.home();
+  };
   render() {
     return (
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>React Hearthstone</h2>
+        <h2 onClick={this.handleClick}>React Hearthstone</h2>
         <ProfileSection />
       </div>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  home: PropTypes.func.isRequired,
+};
+
+export default connect(
+  null,
+  { home },
+)(Header);
